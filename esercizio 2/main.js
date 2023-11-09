bd6854964d7f0feb97fb2f7bffde1ab7f806efa6
DaviGel <geloso.davide@gmail.com>
2023-11-09 16:05:18 +0100

    feat: esercizio dadi

---
+++ esercizio 2/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const numeroUtente = Math.floor(Math.random() * 7);
+console.log('Utente:', numeroUtente);
+const numeroPC = Math.floor(Math.random() * 7);
+console.log('PC', numeroPC);
+
+if (numeroUtente > numeroPC) {
+  console.log('Utente vince!');
+} else if (numeroPC > numeroUtente) {
+  console.log('Computer vince!');
+} else {
+  console.log('La partita è patta!');
+}
