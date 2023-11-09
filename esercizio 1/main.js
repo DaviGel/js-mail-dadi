0989bea03c7caf1338cdd8d9ab35efac9bc51a69
DaviGel <geloso.davide@gmail.com>
2023-11-09 15:50:25 +0100

    feat: esercizio mail

--- esercizio 1/main.js
+++ esercizio 1/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const emailAutorizzate = [
+  'paolo.rossi@gmail.com',
+  'mario.bianchi@gmail.com',
+  'giorgio.verdi@gmail.com',
+];
+
+const emailUtente = prompt('Inserisci la tua e-mail');
+let emailCheck = 0;
+
+for (let i = 0; i < emailAutorizzate.length; i++) {
+  if (emailUtente === emailAutorizzate[i]) {
+    emailCheck = 1;
+    break;
+  }
+}
+
+if (emailCheck === 1) {
+  console.log('E-mail autorizzata!');
+} else {
+  console.log('E-mail non autorizzata!');
+}
